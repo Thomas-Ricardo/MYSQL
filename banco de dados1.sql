399fcf6c161bb7299e0da4215157087d7af4ae70
Thomas-Ricardo <78830945+Thomas-Ricardo@users.noreply.github.com>
2021-09-14 11:35:49 -0300

    passando o banco de dados e as anotações

---
+++ banco de dados1.sql
@@ -0,0 +1,18 @@
+
+insert into cursos values
+('1', 'HTML4', 'CURSO DE HTML5', '40', '37', '2014'),
+('2', 'Algoritimos', 'Lócursosgica de Programação', '20', '15', '2014'),
+('3', 'Photoshop', 'Dicas de Photoshop CC', '10', '8', '2014'),
+('4', 'PGP', 'Curso de PHP para Ininiantes', '40', '20', '2010'),
+('5', 'Jarva', 'Introdução à Linguagem Java', '10', '29', '2000'),
+('6', 'MySQL', 'Banco de Dados MySQL', '30', '15', '2016'),
+('7', 'Word', 'Curso completo de Word', '40', '30', '2016'),
+('8', 'Sapateado', 'Danças Rítmicas', '40', '30', '2018'),
+('9', 'Cozinha Árabe', 'Aprenda a fazer Kibe ', '40', '30', '2018'),
+('10', 'Youtuber', 'Gerar polêmica e ganhar inscritos', '5', '2', '2018');
+
+select * from mafagafos;
+select * from cursos;
+describe mafagafos;
+describe cursos;
+where idcurso='10';
